a59387d5ec41ec036a511e6d9481628de1753042
kihyeoon <kihyeoon@gmail.com>
2025-05-29 09:05:43 +0900

    feat: 탭 아이콘 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,11 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import { colors } from "@/constants";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function TabLayout() {
+  const SIZE = 24;
+
   return (
     <Tabs
       screenOptions={{
@@ -14,18 +17,39 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "홈",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "home" : "home-outline"}
+              color={color}
+              size={SIZE}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="my"
         options={{
           title: "내 프로필",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person-circle" : "person-circle-outline"}
+              color={color}
+              size={SIZE}
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="setting"
         options={{
           title: "설정",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "settings" : "settings-outline"}
+              color={color}
+              size={SIZE}
+            />
+          ),
         }}
       />
     </Tabs>
