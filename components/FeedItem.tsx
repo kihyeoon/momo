3a3e7380f2fd79f77e80a5e3b9c0f58bfea9a7a1
kihyeoon <kihyeoon@gmail.com>
2025-05-30 09:47:03 +0900

    feat: 피드 아이템 컴포넌트 구현

---
+++ components/FeedItem.tsx
@@ -0,0 +1,89 @@
+import { View, StyleSheet, Text, Pressable } from "react-native";
+import { colors } from "@/constants";
+import { MaterialCommunityIcons, Octicons, Ionicons } from "@expo/vector-icons";
+import { useState } from "react";
+import { Post } from "@/types";
+
+const FeedItem = ({ post }: { post: Post }) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text numberOfLines={3} style={styles.description}>
+          {post.description}
+        </Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menu} onPress={() => setIsLiked(!isLiked)}>
+          <Octicons
+            name={isLiked ? "heart-fill" : "heart"}
+            size={16}
+            color={isLiked ? colors.ORANGE_600 : colors.BLACK}
+          />
+          <Text style={isLiked ? styles.activeMenuText : styles.menuText}>
+            {post.likes.length}
+          </Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <MaterialCommunityIcons
+            name="comment-processing-outline"
+            size={16}
+            color={colors.BLACK}
+          />
+          <Text style={styles.menuText}>{post.commentCount}</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <Ionicons name="eye-outline" size={16} color={colors.BLACK} />
+          <Text style={styles.menuText}>{post.viewCount}</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE,
+  },
+  contentContainer: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: colors.BLACK,
+    marginVertical: 8,
+  },
+  description: {
+    fontSize: 14,
+    color: colors.BLACK,
+    marginBottom: 14,
+  },
+  menuContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderTopColor: colors.GRAY_300,
+  },
+  menu: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 4,
+    width: "33%",
+    paddingVertical: 16,
+  },
+  menuText: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+  },
+  activeMenuText: {
+    fontWeight: "500",
+    color: colors.ORANGE_600,
+  },
+});
+
+export default FeedItem;
