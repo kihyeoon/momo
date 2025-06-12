fa724414529f7a97a6dbf208e3b75eea9b8985e1
kihyeoon <kihyeoon@gmail.com>
2025-06-12 09:23:17 +0900

    feat: 설정 화면 구현

--- app/(tabs)/setting/index.tsx
+++ app/(tabs)/setting/index.tsx
@@ -1,6 +1,9 @@
-import { SafeAreaView, Button } from "react-native";
 import AuthRoute from "@/components/AuthRoute";
+import ListItem from "@/components/ListItem";
+import { colors } from "@/constants";
 import useAuth from "@/hooks/queries/useAuth";
+import { Entypo, Octicons } from "@expo/vector-icons";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 
 export default function SettingScreen() {
   const { logout } = useAuth();
@@ -8,8 +11,24 @@ export default function SettingScreen() {
   return (
     <AuthRoute>
       <SafeAreaView>
-        <Button title="로그아웃" onPress={logout} />
+        <View style={styles.space} />
+        <ListItem
+          title="언어 설정"
+          icon={<Entypo name="language" size={16} color={colors.BLACK} />}
+        />
+        <View style={styles.space} />
+        <ListItem
+          title="로그아웃"
+          onPress={logout}
+          icon={<Octicons name="sign-out" size={16} color={colors.BLACK} />}
+        />
       </SafeAreaView>
     </AuthRoute>
   );
 }
+
+const styles = StyleSheet.create({
+  space: {
+    height: 30,
+  },
+});
